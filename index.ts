ab572cb42c957948fb30243b6d65e567a6a6fa98
muralitharan805 <muralitharan805@gmail.com>
2021-09-25 07:56:02 +0530

    1 file modified

--- index.ts
+++ index.ts
@@ -2,8 +2,6 @@ import './style.css';
 
 import { of, map } from 'rxjs';
 
-of('World')
-  .pipe(map((name) => `Hello, ${name}!`))
-  .subscribe(console.log);
+of('World').subscribe(console.log);
 
 // Open the console in the bottom right to see results.
